5dbfa7e8e1ad2fb798483fcfefde24efe32da7c6
jlavado <jlavado@email.com>
2012-12-07 17:04:53 +0100

    ...

---
+++ src/MPE/System.cpp
@@ -0,0 +1,25 @@
+
+#include <MPE/System.hpp>
+
+namespace MPE
+{
+   void System::addEmitter(Emitter::Ptr theEmitter)
+   {
+      if (mEmitters.find(theEmitter->getID()) == mEmitters.end())
+         mEmitters.insert(std::pair<Emitter::ID,Emitter::Ptr>(theEmitter->getID(),theEmitter));
+   }
+
+   void System::addParticle(const Particle& theParticle)
+   {
+      mParticles.push_back(theParticle);
+   }
+
+   void System::update(float theElapsedTime)
+   {
+      for( auto anEmitter: mEmitters)
+      {
+         anEmitter.second->emit
+      }
+   }
+
+}
